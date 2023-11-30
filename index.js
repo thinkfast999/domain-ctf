bc5c7d2de73d7ea8305f032e689259f5fb25c1a3
MK Giga <107154937+mk-giga@users.noreply.github.com>
2023-11-30 03:31:26 +0100

    Update index.js
    structured the code better

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// GET request on '/', aka the home page
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+const PORT = 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+function main() {
+  initHttp();
+  initSocket();
+  listen();
+}
+
+function initSocket() {
+  // fires when a websocket client connects
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    // fires when the message 'test' is sent from the client
+    socket.on('test', () => {
+      // ...
+    });
+  });
+}
+
+function initHttp() {
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+}
+
+function listen() {
+  server.listen(3000, () => {
+    console.log('listening on ' + PORT);
+  });
+}
+
+
+
+
