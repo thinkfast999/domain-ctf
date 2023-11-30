14016a8b819acb8637cc57c342dc86af98d756da
MK Giga <107154937+mk-giga@users.noreply.github.com>
2023-11-30 00:35:55 +0000

    boilerplate

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// GET request on '/', aka the home page
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
